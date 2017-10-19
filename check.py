f8ad25697a409bd39ee05526863968e565ba9f79
Issa Rice <riceissa@gmail.com>
2017-10-19 11:51:04 -0700

    add outline of checks

--- check.py
+++ check.py
@@ -8,19 +8,46 @@ SCRIPTS = glob.glob("proc_*.py")
 
 
 def squeeze_hyphens(s):
-    s = "_".join(filter(bool, s.split('-')))
-    return s
+    return "_".join(filter(bool, s.split('-')))
 
-def check_url(url):
+
+def scriptnames(url):
     filename = url.split('=')[1].split('&')[0]
     root = filename.split('.')[0]
 
     if "MAY" in root or "NOV" in root:
-        scriptname = "proc_" + root + "_adjusted.py"
+        return [
+            "proc_" + root + "_adjusted.py",
+            "proc_" + root + "_original.py",
+        ]
     else:
-        scriptname = "proc_" + squeeze_hyphens(root) + ".py"
+        return ["proc_" + squeeze_hyphens(root) + ".py"]
 
-    assert scriptname in SCRIPTS, filename
+
+def check_url(url):
+    for s in scriptnames(url):
+        assert s in SCRIPTS, url
+
+        with open(s, "r") as f:
+            for line in f:
+                if line.startswith("with open"):
+                    check_csv(line, url, s)
+                elif "www.conference-board.org" in line:
+                    check_dl_link(line, url, s)
+                elif "fps =" in line:
+                    check_glob_csv(line, url, s)
+
+
+def check_csv(line, url, scriptname):
+    pass
+
+
+def check_dl_link(line, url, scriptname):
+    assert url in line, (scriptname, url, line)
+
+
+def check_glob_csv(line, url, scriptname):
+    pass
 
 
 if __name__ == "__main__":
